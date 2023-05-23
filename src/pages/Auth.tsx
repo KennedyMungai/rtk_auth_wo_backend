5a52289081a1bac6e035f6ad5b709fa079ffa670
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-23 14:20:57 +0300

    Updated the width of the section tag

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -14,7 +14,28 @@ const Auth = () => {
     const [formValue, setFormValue] = useState(initialState)
 	const [showRegister, setShowRegister] = useState(false)
 
-	return <section className='vh-100 gradient-custom'></section>
+    return (
+		<section className='vh-100 vw-100 gradient-custom'>
+			<div className='container py-4 h-100'>
+				<div className='row d-flex justify-content-center align-items-center h-100'>
+					<div className='col-12 col-md-8 col-lg-6 col-xl-5'>
+						<div
+							className='card bg-dark text-white'
+							style={{ borderRadius: '1rem' }}
+						>
+							<div className='card-body p-4 text-center'>
+								<div className='mb-md-5 mt-md-4 pb-5'>
+									<h2 className='fw-bold mb-2 text-uppercase'>
+										{!showRegister ? 'Login' : 'Register'}
+									</h2>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	)
 }
 
 export default Auth
