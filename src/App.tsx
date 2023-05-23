4f327883f1d8ca2fd3b2e2b6e9f25ba2551d56f9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-23 13:17:17 +0300

    Set up toastify for the code

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import {
 import './App.css'
 import Auth from './pages/Auth'
 import Dashboard from './pages/Dashboard'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
 	return (
