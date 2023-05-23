00b26d4b00caf89d36454ab72a111e97eaa4663c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-23 13:04:18 +0300

    Imported the material UI css file

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux'
 import App from './App.tsx'
 import { store } from './app/store.ts'
 import './index.css'
+import 'mdb-react-ui-kit/dist/css/mdb.min.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
