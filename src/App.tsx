e0ce63181e9264a2c53201d06e160b996ec45807
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-23 13:12:06 +0300

    Imported the Navigate component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import {
+	BrowserRouter as Router,
+	Route,
+	Routes,
+	Navigate
+} from 'react-router-dom'
 import './App.css'
 
 function App() {
