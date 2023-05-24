91f098ef55f4f86eb210ed0d6820f6d53164f774
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-24 11:30:10 +0300

    Added some simple redirect logic

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -1,11 +1,16 @@
 import { ReactNode } from 'react'
+import { useAppSelector } from '../app/hooks'
+import { selectAuth } from '../features/auth/authSlice'
+import LoadingToRedirect from './LoadingToRedirect'
 
 type Props = {
 	children: ReactNode
 }
 
 const PrivateRoute = ({ children }: Props) => {
-	return <div>PrivateRoute</div>
+	const token = useAppSelector(selectAuth)
+
+	return token ? children : <LoadingToRedirect />
 }
 
 export default PrivateRoute
