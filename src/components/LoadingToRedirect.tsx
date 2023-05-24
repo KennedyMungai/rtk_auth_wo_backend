b7d823bdc3f4e843aff8774e4682263ba6a0fe56
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-24 11:21:27 +0300

    Created a useNavigate instance called navigate

--- src/components/LoadingToRedirect.tsx
+++ src/components/LoadingToRedirect.tsx
@@ -3,7 +3,8 @@ import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 const LoadingToRedirect = () => {
-	const [count, setCount] = useState<number>(5)
+    const [count, setCount] = useState<number>(5)
+	const navigate = useNavigate()
 
 	return <div>LoadingToRedirect</div>
 }
