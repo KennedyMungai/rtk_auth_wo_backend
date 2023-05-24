9bc2cc61ae2775b39ef1846be2dcdcadae693e58
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-24 11:20:02 +0300

    Created the LoadingToRedirect component

---
+++ src/components/LoadingToRedirect.tsx
@@ -0,0 +1,9 @@
+import { MDBModal, MDBModalContent, MDBModalDialog } from 'mdb-react-ui-kit'
+import { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+const LoadingToRedirect = () => {
+	return <div>LoadingToRedirect</div>
+}
+
+export default LoadingToRedirect
